0db13b3ddd505a6e50bec939b06ed523b8967a18
KronosPNG <turcoluigi@outlook.com>
2024-01-05 00:05:53 +0100

    warning toggled off

--- src/utility/model_list.py
+++ src/utility/model_list.py
@@ -9,12 +9,12 @@ from sklearn.neighbors import KNeighborsClassifier
 models = {
     "random_forest" : RandomForestClassifier(n_estimators=100, random_state=42),
     "naive_bayes": GaussianNB(),
-    "svc" : LinearSVC(),
+    "svc" : LinearSVC(dual=True),
     "neural_network": MLPClassifier(random_state=42),
     "gradient_boosting": GradientBoostingClassifier(random_state=0),
     "extra_trees": ExtraTreesClassifier(random_state=0),
     "kneighbors": KNeighborsClassifier(),
-    "list_1": [RandomForestClassifier(n_estimators=100, random_state=42), GaussianNB(), LinearSVC()],
+    "list_1": [RandomForestClassifier(n_estimators=100, random_state=42), GaussianNB(), LinearSVC(dual=True)],
     "list_2": [RandomForestClassifier(n_estimators=100, random_state=42), MLPClassifier(random_state=42), ExtraTreesClassifier(random_state=0)],
     "list_3": [GradientBoostingClassifier(random_state=0), ExtraTreesClassifier(random_state=0), KNeighborsClassifier()]    
 }
