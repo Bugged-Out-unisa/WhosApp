313b2ba06a0ef3b892e4af4ff9a7e1b2ce0f47bc
jose-sgariglia <jose-sgariglia>
2024-01-09 16:04:17 +0100

    - Selezione da command line

---
+++ src/utility/selection_cmdline/datasetSelection.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+from utility.exceptions import ExtensionError
+from simple_term_menu import TerminalMenu
+
+
+class DatasetSelection:
+    DATASET_PATH = "../datasets/"
+
+    def __init__(self):
+        self.__dataset = self.__select_dataset()
+
+    @classmethod
+    def __show_datasets(cls):
+        print("Elenco dei dataset disponibili:")
+        datasets = os.listdir(cls.DATASET_PATH)
+        return datasets
+
+    @classmethod
+    def __load_dataset(cls, index, datasets):
+        if 0 <= index < len(datasets):
+            selected_dataset = datasets[index]
+
+            if not selected_dataset.endswith(".parquet"):
+                raise ExtensionError("Il dataset deve essere in formato .parquet")
+
+            path = f"{cls.DATASET_PATH}{selected_dataset}"
+
+            # Carica il dataset come DataFrame di pandas
+            df = pd.read_parquet(path)
+            return df
+        else:
+            raise ValueError("ID del dataset non valido.")
+
+    @classmethod
+    def __select_dataset(cls):
+        datasets = cls.__show_datasets()
+        menu = TerminalMenu(datasets)
+        menu_entry_index = menu.show()
+
+        dataset_selected = cls.__load_dataset(menu_entry_index, datasets)
+        print(f"Dataset selezionato: {datasets[menu_entry_index]}")
+        return dataset_selected
+
+    @property
+    def dataset(self):
+        return self.__dataset
+
---
+++ src/utility/selection_cmdline/modelSelection.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from utility.model_list import models
+from simple_term_menu import TerminalMenu
+from utility.exceptions import ExtensionError
+
+
+class ModelSelection:
+    MODEL_PATH = "../models/"
+    model_names = [name for name in models.keys()]
+
+    def __init__(self):
+        self.__model = self.__select_model()
+
+    @classmethod
+    def __load_model(cls, model_name):
+        if model_name not in models.keys():
+            raise ValueError("Modello non valido.")
+
+        selected_model_name = models[model_name]
+
+        return selected_model_name
+
+    @classmethod
+    def __select_model(cls):
+        print("Elenco dei modelli disponibili:")
+        menu = TerminalMenu(cls.model_names)
+        menu_entry_index = menu.show()
+
+        model_name = cls.model_names[menu_entry_index]
+
+        model_selected = cls.__load_model(model_name)
+        print(f"Modello selezionato: {model_name}")
+        return model_selected
+
+    @property
+    def model(self):
+        return self.__model
+
