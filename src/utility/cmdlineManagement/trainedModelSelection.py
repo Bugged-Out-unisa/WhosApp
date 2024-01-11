27fc0fe3ecff6a54793af2be961c20c4e5e24bff
jose-sgariglia <jose-sgariglia>
2024-01-11 16:21:53 +0100

    - Permette di selezionare solo i file

--- src/utility/cmdlineManagement/trainedModelSelection.py
+++ src/utility/cmdlineManagement/trainedModelSelection.py
@@ -17,8 +17,12 @@ class TrainedModelSelection:
     @classmethod
     def __show_models(cls):
         print("Elenco dei modelli disponibili:")
-        models = [model for model in os.listdir(cls.MODEL_PATH)
-                  if model.endswith(".skops") or model.endswith(".onnx")]
+
+        # Seleziona solo i file
+        all_file = [file for file in os.listdir(cls.MODEL_PATH) if os.path.isfile(os.path.join(cls.MODEL_PATH, file))]
+
+        # Seleziona solo i modelli
+        models = [model for model in all_file if model.endswith(".skops") or model.endswith(".onnx")]
 
         # Ordina i modelli in base alla data di creazione
         models = sorted(models, key=lambda x: os.path.getctime(os.path.join(cls.MODEL_PATH, x)), reverse=True)
