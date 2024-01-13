5843d33f5110113cdaabd5797f0aa61a5132f12c
jose-sgariglia <jose-sgariglia>
2024-01-13 12:09:59 +0100

    Aggiunta blocco try-catch per prendere l'eccezione `KeyboardInterrupt`

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from utility.dataset.featureConstruction import featureConstruction
 from utility.cmdlineManagement.trainedModelSelection import TrainedModelSelection
+
 
 class modelExecution:
     """
@@ -13,7 +14,7 @@ class modelExecution:
     MODEL_PATH = "../models/"
 
     def __init__(self, pipeline = None):
-        if pipeline == None:
+        if pipeline is None:
             pipeline = TrainedModelSelection().model
 
         # Carica il modello e lo scaler dalla pipeline
@@ -35,34 +36,38 @@ class modelExecution:
             Assegna delle probabilità anche in base allo storico di tutti i messaggi inseriti.
         """
 
-        # Per ogni utente da prevedere, crea una lista di previsioni
-        num_users = self.__trainedModel.n_classes_
-        predictions = [[] for _ in range(num_users)]
+        try:
+            # Per ogni utente da prevedere, crea una lista di previsioni
+            num_users = self.__trainedModel.n_classes_
+            predictions = [[] for _ in range(num_users)]
 
-        while True:
+            while True:
 
-            # Ottieni il messaggio da input
-            message = input("\nScrivi un messaggio:\n")
+                # Ottieni il messaggio da input
+                message = input("\nScrivi un messaggio:\n")
 
-            # Crea il dataframe e costruisci le feature su quel messaggio
-            df = self.dataframe_for_messages([message])
+                # Crea il dataframe e costruisci le feature su quel messaggio
+                df = self.dataframe_for_messages([message])
 
-            # Ottieni probabilità per ogni utente
-            # [0] perché resituisce una lista di previsioni (come se si aspettasse più messaggi)
-            users_prob = self.__trainedModel.predict_proba(df)[0]
+                # Ottieni probabilità per ogni utente
+                # [0] perché resituisce una lista di previsioni (come se si aspettasse più messaggi)
+                users_prob = self.__trainedModel.predict_proba(df)[0]
 
-            # Per ogni utente, ottieni probabilità per il messaggio inserito e salva in lista
-            for i in range(num_users):
-                predictions[i].append(users_prob[i])
+                # Per ogni utente, ottieni probabilità per il messaggio inserito e salva in lista
+                for i in range(num_users):
+                    predictions[i].append(users_prob[i])
 
-            # Stampa report
-            # Solo per l'ultima previsione [-1]
-            print("\n---SINGOLO---")
-            print("\n".join([f"USER {i}: {predictions[i][-1]:.2f}" for i in range(num_users)]))
+                # Stampa report
+                # Solo per l'ultima previsione [-1]
+                print("\n---SINGOLO---")
+                print("\n".join([f"USER {i}: {predictions[i][-1]:.2f}" for i in range(num_users)]))
 
-            # Media delle previsioni
-            print("\n----MEDIA----")
-            print("\n".join([f"USER {i}: {np.average(predictions[i]):.2f}" for i in range(num_users)]))
+                # Media delle previsioni
+                print("\n----MEDIA----")
+                print("\n".join([f"USER {i}: {np.average(predictions[i]):.2f}" for i in range(num_users)]))
+        except KeyboardInterrupt:
+            print("\n\n[INFO] Interruzione dell'esecuzione del modello.")
+            exit(0)
 
 
 if __name__ == "__main__":
