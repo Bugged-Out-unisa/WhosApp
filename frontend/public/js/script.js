12ad0dc637a2db0337041516cf0e59ce07791399
KronosPNG <turcoluigi@outlook.com>
2024-01-14 17:08:23 +0100

    commenti

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,21 +1,26 @@
 $(function() {
     const userInput = $("#user-input");
     const isThinking = $("#is-thinking");
-    const emojiPicker = $("#emoji-picker");
+    const emojiPicker = $("emoji-picker");
+    
+    // Audio per la notifica di invio e ricezione messaggio
     const msgSent = new Audio('/assets/sent.mp3');
     const msgArrived = new Audio('/assets/arrived.mp3');
+
+    // Variabile per evitare l'invio di più messaggi contemporaneamente
     let isWaiting = false;
 
-    isThinking.css("opacity", "0");
-
+    // Listener per l'aggiunzione di un emoji al messaggio
     emojiPicker.on("emoji-click", (event) => {
         userInput.val(userInput.val() + event.detail.unicode);
     });
 
+    // Listener per l'apertura e la chiusura del picker
     $("#emoji-btn").on("click", () => {
         emojiPicker.toggle();
     });
 
+    // Funzione per l'invio del messaggio al modello
     const getResponse = (text) => {
         $.ajax({
             url: "/getResponse",
@@ -38,6 +43,7 @@ $(function() {
         });
     }
 
+    // Funzione per la stampa del messaggio dell'utente e invio al backend
     const sendMessage = (text) => {
         text = text.trim();
         if(text == "" || (/^[\n\r]*$/.test(text))) return;
@@ -46,6 +52,8 @@ $(function() {
         text = text.replace(/\n/g, "\\n");
 
         userInput.val("");
+
+        emojiPicker.hide();
         
         $.ajax({
             url: "/newUserMessage",
@@ -66,13 +74,16 @@ $(function() {
           });
     }
 
+    // Listener per l'invio del messaggio tramite pulsante
     $("#app").on("click", "#send-btn", (e) => {
         sendMessage(userInput.val())
     })
 
+    // Listener per l'invio del messaggio tramite tasto invio
     $("#app").on("keypress", "#user-input", (e) => {
         var id = e.key || e.which || e.keyCode || 0; 
-                
+        
+        // 13 = tasto invio
         if ((id == 13  || id == "Enter") && !e.shiftKey) {
             e.preventDefault();
             sendMessage(userInput.val())
