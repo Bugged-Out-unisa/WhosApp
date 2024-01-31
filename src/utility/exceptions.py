105f775dccf0b211aae3c7b84bd832107636fbe3
jose-sgariglia <jose-sgariglia>
2024-01-31 21:32:06 +0100

    Creazione per notificare l'utente che il data_frame può essere solo generato

--- src/utility/exceptions.py
+++ src/utility/exceptions.py
@@ -11,3 +11,7 @@ class DatasetNotFoundError(Exception):
 class ModelNotFoundError(Exception):
     """Raised when the model is not found"""
     pass
+
+
+class SetterNotAllowedError(Exception):
+    """Raised when an attempt is made to set a class attribute when it is not allowed"""
